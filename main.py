dd031d7e7574291814a773f23733ac83339d7d79
Person0z <80363995+Person0z@users.noreply.github.com>
2023-06-02 19:25:21 -0500

    Too much

--- main.py
+++ main.py
@@ -3,23 +3,18 @@ import aiohttp
 import asyncio
 from datetime import datetime
 import json
+import aioping
 
 app = Flask(__name__)
-
-# Read list of servers from JSON file
-with open('servers.json', 'r') as f:
-    servers = json.load(f)
 
 # Dictionary to hold server status
 server_status = {}
 
-@app.route('/api/monitor', methods=['GET'])
-def monitor():
-    # Run the asynchronous function and wait for it to complete
-    asyncio.run(check_servers())
-    return jsonify(server_status)
-
 async def check_servers():
+    # Read list of servers from JSON file
+    with open('servers.json', 'r') as f:
+        servers = json.load(f)
+
     # aiohttp works best with a session for multiple requests
     async with aiohttp.ClientSession() as session:
         # Create a list of tasks to run asynchronously
@@ -27,22 +22,42 @@ async def check_servers():
         # Run the tasks and wait for all of them to complete
         await asyncio.gather(*tasks)
 
+@app.route('/api/monitor', methods=['GET'])
+def monitor():
+    # Run the asynchronous function and wait for it to complete
+    asyncio.run(check_servers())
+    return jsonify(server_status)
+
 async def check_server(session, server):
     try:
-        async with session.get(server) as response:
-            status = response.status
-            if status == 200:
+        # If server is IP address
+        if server.replace('.', '').isnumeric():
+            try:
+                await aioping.ping(server)  # Ping the server
                 if server_status.get(server, {}).get('status') != 'running':
                     server_status[server] = {'status': 'running', 'since': datetime.now().isoformat()}
-            else:
+            except TimeoutError:
                 if server_status.get(server, {}).get('status') != 'not running':
                     server_status[server] = {'status': 'not running', 'since': datetime.now().isoformat()}
+        else:
+            async with session.get(server) as response:
+                status = response.status
+                if status == 200:
+                    if server_status.get(server, {}).get('status') != 'running':
+                        server_status[server] = {'status': 'running', 'since': datetime.now().isoformat()}
+                else:
+                    if server_status.get(server, {}).get('status') != 'not running':
+                        server_status[server] = {'status': 'not running', 'since': datetime.now().isoformat()}
     except:
         if server_status.get(server, {}).get('status') != 'not running':
             server_status[server] = {'status': 'not running', 'since': datetime.now().isoformat()}
 
 @app.route('/')
 def home():
+    # Clear the server status before updating
+    server_status.clear()
+    # Run the asynchronous function and wait for it to complete
+    asyncio.run(check_servers())
     return render_template('index.html')
 
 if __name__ == '__main__':
--- static/script.js
+++ static/script.js
@@ -1,4 +1,47 @@
-setInterval(monitor, 5000);
+window.onload = function() {
+    const darkModeToggle = document.getElementById('darkModeToggle');
+
+    // Check if a preference cookie exists
+    const modePreference = getCookie('modePreference');
+    if (modePreference === 'dark') {
+        darkModeToggle.checked = true;
+        document.body.classList.add('dark-mode');
+    }
+
+    darkModeToggle.addEventListener('change', function() {
+        if (this.checked) {
+            document.body.classList.add('dark-mode');
+            setCookie('modePreference', 'dark', 365); // Set preference cookie for 1 year
+        } else {
+            document.body.classList.remove('dark-mode');
+            setCookie('modePreference', 'light', 365); // Set preference cookie for 1 year
+        }
+    });
+
+    monitor();
+    setInterval(monitor, 30000);
+}
+
+// Function to set a cookie
+function setCookie(name, value, days) {
+    const date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    const expires = 'expires=' + date.toUTCString();
+    document.cookie = name + '=' + value + ';' + expires + ';path=/';
+}
+
+// Function to get a cookie value
+function getCookie(name) {
+    const cookieName = name + '=';
+    const cookies = document.cookie.split(';');
+    for (let i = 0; i < cookies.length; i++) {
+        let cookie = cookies[i].trim();
+        if (cookie.indexOf(cookieName) === 0) {
+            return cookie.substring(cookieName.length, cookie.length);
+        }
+    }
+    return '';
+}
 
 function monitor() {
     fetch('/api/monitor')
